7b907ce2edca800b1c9ca175392b01979825da8e
Voltolini <lucas.cvoltolini@gmail.com>
2023-03-30 19:08:55 -0300

    Modificação para o projeto final

--- Projetos/Calculadora/__init__.py
+++ Projetos/Calculadora/__init__.py
@@ -1,4 +1,7 @@
-from funcoes import *
+# from . import funcoes
+#setup.py setup tools -> criacao de modulos para arquivos de fora .py
+# https://deepnote.com/
+from Calculadora import funcoes
 
 def calcule():
     pass
